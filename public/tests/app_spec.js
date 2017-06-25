8a5fe7a5d782b5c8aaa1d7b5f247eb9a0dbb389f
イケガミ <ikgmtds@gmail.com>
2017-06-25 17:27:55 +0900

    2300

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -6,5 +6,10 @@ describe('LearnJS',function(){
     it('show the landing page view when there is no hash',function(){
         learnjs.showView('');
         expect($('.view-container .landing-view').length).toEqual(1);
+    });
+    it('passes the hash view parameter to the view funtion',function(){
+        spyOn(learnjs,'problemView');
+        learnjs.showView('#problem-42');
+        expect(learnjs.problemView).toHaveBeenCalledWith('42');
     })
 });
